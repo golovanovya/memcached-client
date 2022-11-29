f55640ecbfdfb0d14b33c6f53217aef61168ff65
golovanovya <golovanovya@gmail.com>
2022-11-29 11:08:07 +0300

    fix memcached address for ci tests

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -8,14 +8,14 @@ final class ClientTest extends TestCase
 {
     public function testGetSetValue()
     {
-        $client = new Client('localhost', 11211);
+        $client = new Client('memcached', 11211);
         $client->set("key1", "val1");
         $this->assertEquals("val1", $client->get("key1"));
     }
 
     public function testGetNotSaved()
     {
-        $client = new Client('localhost', 11211);
+        $client = new Client('memcached', 11211);
         $this->assertNull($client->get("key2"));
         $this->assertEquals("test", $client->get("key2", fn () => "test"));
     }
